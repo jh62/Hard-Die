4d050848b05283c637bdfd169675c8dcdbb3fbea
Paul <marton.maps@gmail.com>
2022-09-22 16:07:10 -0300

    Fixed animations for some states
    Unnarmed and pistol animations now transition more naturally.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,65 +4,17 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 0f;
-
-    private float accel = 4.25f;
-
     [SerializeField]
-    private Animator animator;
-
-    [SerializeField]
-    private CharacterController controller;
-
-    void Start()
-    {
-
-    }
-
-    float pistolWeight = 0f;
-    float rifleWeight = 0f;
-    float unnarmedWeight = 1f;
-
-    public int weapon = 0;
-
+    private HumanBodyController movementController;
 
     void Update()
     {
-        // animator.SetLayerWeight(1, pistolWeight);
-        // animator.SetLayerWeight(2, rifleWeight);
-        // animator.SetLayerWeight(3, unnarmedWeight);
+        movementController.setShooting(Input.GetButton("Fire1"));
 
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        float _x = Input.GetAxis("Horizontal");
+        float _z = Input.GetAxis("Vertical");
+        bool sprinting = Input.GetKey(KeyCode.LeftShift);
 
-        if (z != 0f || x != 0f)
-        {
-            bool sprinting = Input.GetKey(KeyCode.LeftShift);
-            float sprint = sprinting ? accel : accel * .75f;
-            speed = Mathf.Clamp(speed + sprint * Time.deltaTime, 0f, 1f);
-
-            if (!sprinting && speed > .5f)
-                speed -= .025f;
-        }
-        else
-        {
-            speed *= .217f;
-        }
-
-
-        if (speed < Mathf.Epsilon)
-        {
-            speed = 0f;
-            Debug.Log("Epsiloneado");
-        }
-
-        if (speed >= .47f)
-        {
-            Vector3 move = transform.right * x + transform.forward * z;
-            controller.Move(move * accel * speed * Time.deltaTime);
-        }
-
-        animator.SetFloat("x", speed * x);
-        animator.SetFloat("y", speed * z);
+        movementController.Move(_x, _z, sprinting);
     }
 }
